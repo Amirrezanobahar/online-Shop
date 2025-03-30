3e19c04844327c139ef22a640a14456d998f9f56
Amirrezanobahar <nwbharamyrrda@gmail.com>
2025-03-30 16:42:11 +0330

    add EventSlider

--- online-shop/src/App.jsx
+++ online-shop/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/header/Header';
+import EventSlider from './components/EventNavbar/EventSlider';
 
 function App() {
 
   return (
 <Router>
 <Header/>
+<EventSlider/>
 
 <Routes>
 
