a46dce1bc0457b323535bafdba5ce461c0434730
Amirrezanobahar <nwbharamyrrda@gmail.com>
2025-03-30 17:16:12 +0330

    add specialOffer

--- online-shop/src/App.jsx
+++ online-shop/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/header/Header';
 import EventSlider from './components/EventNavbar/EventSlider';
+import SpecialOffers from './components/specialOffers/SpecialOffers';
 
 function App() {
 
   return (
 <Router>
+
 <Header/>
 <EventSlider/>
+<SpecialOffers/>
 
 <Routes>
 
