b23fc4709adf0c372502d6a7e89a1b35187aa8cf
Amirrezanobahar <nwbharamyrrda@gmail.com>
2025-04-07 15:41:07 +0330

    i create login component

--- online-shop/src/App.jsx
+++ online-shop/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/footer/footer';
-import Register from './components/register/Register';
+import Register from './components/auth/register/Register';
+import Login from './components/auth/login/Login'
 import Body from './components/body/Body';
 import Header from './components/header/Header'
 
@@ -16,6 +17,7 @@ function App() {
 <Routes>
   <Route path="/" element={<Body/>} />
 <Route path="/register" element={<Register/>} />
+<Route path ="/login" element={<Login/>}/>
 
 </Routes>
 
